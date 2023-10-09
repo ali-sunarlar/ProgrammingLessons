253c7b21fcbc7e2c85a9bac131672b10bbe81d60
Ali Sunarlar <sunarlarali@outlook.com>
2023-10-09 21:37:51 +0300

    modified:   WebProgramming/WebFormUyg/WebFormUyg/Controllers/OgrenciController.cs modified:   WebProgramming/WebFormUyg/WebFormUyg/Views/Ogrenci/Index.cshtml

--- WebProgramming/WebFormUyg/WebFormUyg/Controllers/OgrenciController.cs
+++ WebProgramming/WebFormUyg/WebFormUyg/Controllers/OgrenciController.cs
@@ -23,16 +23,16 @@ namespace WebFormUyg.Controllers
             return txt;
         }
 
-        /*[HttpGet]
+        [HttpGet]
         public string Kaydet2()
         {
             string txt;
-            string ad = HttpContext.Request.Query["ad"];
-            string soyad = HttpContext.Request.Query["soyad"];
-            string numara = HttpContext.Request.Query["numara"];
+            string ad = HttpContext.Request.Query["OgrAd"];
+            string soyad = HttpContext.Request.Query["Ogrsoyad"];
+            string numara = HttpContext.Request.Query["OgrNo"];
             txt = ad + " " + soyad + " " + numara;
             return txt;
-        }*/
+        }
 
         [HttpPost] //Post ile gelmemişse Kaydet metodu'u çalışmayacaktır.
         public string Kaydet3(Ogrenci ogr)
