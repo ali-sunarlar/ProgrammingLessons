a2e5b41239fe5f4a77fc89a28bb9ff1879928d17
Ali Sunarlar <sunarlarali@outlook.com>
2023-10-12 01:09:58 +0300

    modified:   MobileProgramming/ders4_1/lib/main.dart

--- MobileProgramming/ders4_1/lib/main.dart
+++ MobileProgramming/ders4_1/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main(){
+/* void main(){
   runApp(
     const Center(
       child: Text(
@@ -9,4 +9,168 @@ void main(){
       ),
     ),
   );
+} */
+
+
+/* void main(){
+  runApp(new GreenFrog());
+}
+
+class GreenFrog extends StatelessWidget {
+  const GreenFrog({ super.key });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(color: const Color(0xFF2DBD3A));
+  }
+} */
+
+/* void main(){
+  runApp(new Frog());
+}
+class Frog extends StatelessWidget {
+  const Frog({
+    super.key,
+    this.color = const Color(0xFF2DBD3A),
+    this.child,
+  });
+
+  final Color color;
+  final Widget? child;
+
+  @override
+  Widget build(BuildContext context) {
+    return ColoredBox(color: color, child: child);
+  }
+} */
+
+
+/* void main(){
+  runApp(new Bird());
+}
+class Bird extends StatefulWidget {
+  const Bird({
+    super.key,
+    this.color = const Color(0xFFFFE306),
+    this.child,
+  });
+
+  final Color color;
+  final Widget? child;
+
+  @override
+  State<Bird> createState() => _BirdState();
+}
+
+class _BirdState extends State<Bird> {
+  double _size = 1.0;
+
+  void grow() {
+    setState(() { _size += 0.1; });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: widget.color,
+      transform: Matrix4.diagonal3Values(_size, _size, 1.0),
+      child: widget.child,
+    );
+  }
+} */
+
+/* import 'package:flutter/material.dart';
+
+/// Flutter code sample for [Scaffold].
+
+void main() => runApp(const ScaffoldExampleApp());
+
+class ScaffoldExampleApp extends StatelessWidget {
+  const ScaffoldExampleApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: ScaffoldExample(),
+    );
+  }
+}
+
+class ScaffoldExample extends StatefulWidget {
+  const ScaffoldExample({super.key});
+
+  @override
+  State<ScaffoldExample> createState() => _ScaffoldExampleState();
+}
+
+class _ScaffoldExampleState extends State<ScaffoldExample> {
+  int _count = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Example Title'),
+        titleTextStyle: Theme.of(context)
+        .primaryTextTheme
+        .headline6,
+      ),
+      body: Center(child: Text('You have pressed the button $_count times.')),
+      backgroundColor: Colors.blueGrey.shade200,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => setState(() => _count++),
+        tooltip: 'Increment Counter',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+} */
+
+
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      title: 'Flutter Tutorial',
+      home: TutorialHome(),
+    ),
+  );
+}
+
+class TutorialHome extends StatelessWidget {
+  const TutorialHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // Scaffold is a layout forv
+    // the major Material Components.
+    return Scaffold(
+      appBar: AppBar(
+        leading: const IconButton(
+          icon: Icon(Icons.menu),
+          tooltip: 'Navigation menu',
+          onPressed: null,
+        ),
+        title: const Text('Example title'),
+        actions: const [
+          IconButton(
+            icon: Icon(Icons.search),
+            tooltip: 'Search',
+            onPressed: null,
+          ),
+        ],
+      ),
+      // body is the majority of the screen.
+      body: const Center(
+        child: Text('Hello, world!'),
+      ),
+      floatingActionButton: const FloatingActionButton(
+        tooltip: 'Add', // used by assistive technologies
+        onPressed: null,
+        child: Icon(Icons.add),
+      ),
+    );
+  }
 }
