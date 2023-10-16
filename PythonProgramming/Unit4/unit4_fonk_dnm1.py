db0a8e99d4906f9adef4beb6d1f9bab1fcb3ec4d
Ali Sunarlar <sunarlarali@outlook.com>
2023-10-16 19:27:56 +0300

    new file:   PythonProgramming/Unit3/unit3_for_if_ornek_1.py new file:   PythonProgramming/Unit4/unit4_fonk_dnm1.py new file:   PythonProgramming/Unit4/unit4_fonk_dnm2.py

---
+++ PythonProgramming/Unit4/unit4_fonk_dnm1.py
@@ -0,0 +1,5 @@
+def hesapla(sayi1,sayi2):
+    toplam=sayi1+sayi2
+    
+toplam  =hesapla(5,7)
+print(toplam)
---
+++ PythonProgramming/Unit4/unit4_fonk_dnm2.py
@@ -0,0 +1,9 @@
+def hesapla(sayi1=12,sayi2=7,operator="+"):
+    if operator=="+":
+        toplam=sayi1+sayi2
+    if operator=="-":
+        toplam=sayi1-sayi2
+    else:
+        sonuc  ="hesaplayamadim"
+        return sonuc
+toplam=hesapla(sayi2=2)
