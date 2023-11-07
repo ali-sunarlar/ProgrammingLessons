2a2ab3702d047f2c8a7617d02592bf4ee0a40dc3
Ali Sunarlar <sunarlarali@outlook.com>
2023-11-07 12:02:11 +0300

    full-stack

---
+++ Full-StackProgramming/donguler9/donguler9/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace donguler9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int adet = 0; 
+            Console.Write("Sayi: giriniz:"); 
+            int sayi = Convert.ToInt32(Console.ReadLine()); 
+            for (int i = 75; i <= 300; i++) 
+            { 
+                if (i % sayi == 0) 
+                { 
+                    adet++; 
+                } 
+            }
+            Console.WriteLine("Tam Bölünen Sayı Adedi:" + adet); 
+            Console.ReadKey();
+        }
+    }
+}
