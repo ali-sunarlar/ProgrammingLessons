b0ae82cc5348575646a620f8925f26c943f4b91c
Ali Sunarlar <sunarlarali@outlook.com>
2023-11-22 12:56:36 +0300

    new file:   Full-StackProgramming/Diziler1/Diziler1.sln new file:   Full-StackProgramming/Diziler1/Diziler1/App.config new file:   Full-StackProgramming/Diziler1/Diziler1/Diziler1.csproj new file:   Full-StackProgramming/Diziler1/Diziler1/Program.cs new file:   Full-StackProgramming/Diziler1/Diziler1/Properties/AssemblyInfo.cs new file:   Full-StackProgramming/Diziler1/Diziler2/App.config new file:   Full-StackProgramming/Diziler1/Diziler2/Diziler2.csproj new file:   Full-StackProgramming/Diziler1/Diziler2/Program.cs new file:   Full-StackProgramming/Diziler1/Diziler2/Properties/AssemblyInfo.cs new file:   Full-StackProgramming/Diziler1/Diziler3/App.config new file:   Full-StackProgramming/Diziler1/Diziler3/Diziler3.csproj new file:   Full-StackProgramming/Diziler1/Diziler3/Program.cs new file:   Full-StackProgramming/Diziler1/Diziler3/Properties/AssemblyInfo.cs new file:   Full-StackProgramming/Diziler1/Diziler4/App.config new file:   Full-StackProgramming/Diziler1/Diziler4/Diziler4.csproj new file:   Full-StackProgramming/Diziler1/Diziler4/Program.cs new file:   Full-StackProgramming/Diziler1/Diziler4/Properties/AssemblyInfo.cs new file:   Full-StackProgramming/Diziler1/Diziler5/App.config new file:   Full-StackProgramming/Diziler1/Diziler5/Diziler5.csproj new file:   Full-StackProgramming/Diziler1/Diziler5/Program.cs new file:   Full-StackProgramming/Diziler1/Diziler5/Properties/AssemblyInfo.cs new file:   Full-StackProgramming/Diziler1/Diziler6/App.config new file:   Full-StackProgramming/Diziler1/Diziler6/Diziler6.csproj new file:   Full-StackProgramming/Diziler1/Diziler6/Program.cs new file:   Full-StackProgramming/Diziler1/Diziler6/Properties/AssemblyInfo.cs new file:   Full-StackProgramming/Diziler1/Diziler7/App.config new file:   Full-StackProgramming/Diziler1/Diziler7/Diziler7.csproj new file:   Full-StackProgramming/Diziler1/Diziler7/Program.cs new file:   Full-StackProgramming/Diziler1/Diziler7/Properties/AssemblyInfo.cs

---
+++ Full-StackProgramming/Diziler1/Diziler4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diziler4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //dizideki negatif sayıları pozitif sayılara çeviren programı yazınız
+            // (eksi değerler girilmesi istensin)
+
+            Console.Write("Dizinin Eleman Sayısını giriniz: "); 
+            int elemanSayisi = Convert.ToInt32(Console.ReadLine());
+            int[] sayiDizisi = new int[elemanSayisi]; 
+            
+            for (int i = 0; i < sayiDizisi.Length; i++)
+            { 
+                Console.Write("Negatif Sayi Giriniz: "); 
+                sayiDizisi[i] = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(sayiDizisi[i] * -1); 
+            }
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
