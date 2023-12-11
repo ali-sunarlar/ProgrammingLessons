d22ba5b4ec26b2e6dff23286912de33d52aaff85
Ali Sunarlar <sunarlarali@outlook.com>
2023-12-11 20:46:02 +0300

    new file:   MobileProgramming/.gitignore modified:   WebProgramming/.gitignore new file:   WebProgramming/TagHelper/TagHelper.sln new file:   WebProgramming/TagHelper/TagHelper/Controllers/HomeController.cs new file:   WebProgramming/TagHelper/TagHelper/Controllers/OgrenciController.cs new file:   WebProgramming/TagHelper/TagHelper/Models/ErrorViewModel.cs new file:   WebProgramming/TagHelper/TagHelper/Program.cs new file:   WebProgramming/TagHelper/TagHelper/Properties/launchSettings.json new file:   WebProgramming/TagHelper/TagHelper/TagHelper.csproj new file:   WebProgramming/TagHelper/TagHelper/Views/Home/Index.cshtml new file:   WebProgramming/TagHelper/TagHelper/Views/Home/Privacy.cshtml new file:   WebProgramming/TagHelper/TagHelper/Views/Shared/Error.cshtml new file:   WebProgramming/TagHelper/TagHelper/Views/Shared/_Layout.cshtml new file:   WebProgramming/TagHelper/TagHelper/Views/Shared/_Layout.cshtml.css new file:   WebProgramming/TagHelper/TagHelper/Views/Shared/_ValidationScriptsPartial.cshtml new file:   WebProgramming/TagHelper/TagHelper/Views/_ViewImports.cshtml new file:   WebProgramming/TagHelper/TagHelper/Views/_ViewStart.cshtml new file:   WebProgramming/TagHelper/TagHelper/appsettings.Development.json new file:   WebProgramming/TagHelper/TagHelper/appsettings.json

---
+++ WebProgramming/TagHelper/TagHelper/Controllers/OgrenciController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TagHelper.Controllers
+{
+    public class OgrenciController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
