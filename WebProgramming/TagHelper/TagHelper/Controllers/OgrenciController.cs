bc31eff026ef062f101eb5997587b8d6e8d0b755
Ali Sunarlar <sunarlarali@outlook.com>
2023-12-11 22:20:43 +0300

    TagHelper

--- WebProgramming/TagHelper/TagHelper/Controllers/OgrenciController.cs
+++ WebProgramming/TagHelper/TagHelper/Controllers/OgrenciController.cs
@@ -1,12 +1,35 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
 
 namespace TagHelper.Controllers
 {
     public class OgrenciController : Controller
     {
-        public IActionResult Index()
+
+        public IActionResult OgrEkle() 
         {
+            ViewBag.SehirKod = new List<SelectListItem>
+            {
+                new SelectListItem {Value="06",Text="Ankara"},
+                new SelectListItem {Value="34",Text="Istanbul"},
+                new SelectListItem {Value="42",Text="Konya"},
+                new SelectListItem {Value="32",Text="Isparta"}
+
+            };
+            
+            return View(); 
+        
+        }
+
+        [HttpPost]
+        public IActionResult OgrKaydet(OgrenciController ogr)
+        {
+            //Kaydet vb Islemler
+            ViewBag.KayitDurum = "Kayit Islemi Basarili";
             return View();
         }
+
+
     }
 }
---
+++ WebProgramming/TagHelper/TagHelper/Models/Ogrenci.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TagHelper.Models
+{
+    public class Ogrenci
+    {
+        [Display(Name="Öğrenci Adı")]
+        public string OgrAd { get; set; }
+        public string OgrSoyad { get; set; }
+        public string OgrNo { get; set; }
+        public string Sehirler { get; set; }
+        public string DogumTarihi { get; set; }
+
+
+
+
+    }
+}
