1986bc88cdc073f346811fb563ec812cbada3bb8
Ali Sunarlar <sunarlarali@outlook.com>
2023-12-16 22:03:17 +0300

    new file:   "PythonProgramming/6. MOD\303\234LLER - 1/pip_information.py"

---
+++ PythonProgramming/6. MODÜLLER - 1/pip_information.py
@@ -0,0 +1,9 @@
+
+#Komut              Görevi
+#install            Yeni bir paket yükler.
+#uninstall          Varolan bir paketi siler.
+#freeze             Yüklü tüm paketleri requirements formatında listesini çıktıya verir.
+#list               Yüklü tüm paketleri normal listesini çıktıya verir.
+#show               Yüklü paketler hakkında bilgi verir.
+#search             Paketler içinde arama yapar.
+#wheel              Paketler içinde requirements için bir arşiv yapar.
